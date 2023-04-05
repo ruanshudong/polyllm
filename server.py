dacec1209f7b4f72b52f420c72e4c27c10e40278
ruanshudong <ruanshudong@qq.com>
2023-04-05 18:48:22 +0800

    add handle_stream

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ def torch_gc():
             torch.cuda.empty_cache()
             torch.cuda.ipc_collect()
 
+
 path_or_name = "chatglm-6b-int4-qe"
 tokenizer = AutoTokenizer.from_pretrained(
     path_or_name, trust_remote_code=True)
@@ -42,5 +43,37 @@ async def handle(websocket, path):
         torch_gc()
         await websocket.send(json.dumps(answer))
 
-asyncio.get_event_loop_policy().get_event_loop().run_until_complete(websockets.serve(handle, '0.0.0.0', 8765))
+
+async def handle_stream(websocket, path):
+    async for message in websocket:
+        print(message)
+        message_dict = json.loads(message)
+        for response, history in model.stream_chat(tokenizer, message_dict["prompt"], history=message_dict["history"], max_length=message_dict.get(
+                "max_length", 2048),
+                top_p=message_dict.get("top_p", 0.7),
+                temperature=message_dict.get("temperature", 0.7)):
+
+            print(response)
+            print(history)
+            # query, response = history[-1]
+            answer = {
+                "prompt": response,
+                "history": [],
+                "eof": False,
+            }
+
+            await websocket.send(json.dumps(answer))
+
+            answer = {
+                "prompt": "",
+                "history": [],
+                "eof": True,
+            }
+
+            await websocket.send(json.dumps(answer))
+
+        torch_gc()
+
+asyncio.get_event_loop_policy().get_event_loop().run_until_complete(
+    websockets.serve(handle, '0.0.0.0', 8765))
 asyncio.get_event_loop().run_forever()
