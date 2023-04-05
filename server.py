8427f679f326ee27f0491afc400b7db4c2a56990
ruanshudong <ruanshudong@qq.com>
2023-04-05 16:30:50 +0800

    fix message to json

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from transformers import AutoTokenizer, AutoModel
 import asyncio
 import websockets
+import json
 
 
 path_or_name = "chatglm-6b-int4-qe"
@@ -15,20 +16,23 @@ model.eval()
 async def handle(websocket, path):
     async for message in websocket:
         print(message)
+        message_dict = json.loads(message)
         response, history = model.chat(tokenizer,
-                                       prompt,
-                                       history=message.history,
-                                       max_length=message.max_length if message.max_length else 2048,
-                                       top_p=message.top_p if message.top_p else 0.7,
-                                       temperature=message.temperature if message.temperature else 0.95)
+                                       message_dict["prompt"],
+                                       history=message_dict["history"],
+                                       max_length=message_dict.get(
+                                           "max_length", 2048),
+                                       top_p=message_dict.get("top_p", 0.7),
+                                       temperature=message_dict.get("temperature", 0.7))
         answer = {
             "prompt": response,
             "history": history,
         }
         torch_gc()
-        await websocket.send(message)
+        await websocket.send(json.dumps(answer))
 
-
-asyncio.get_event_loop().run_until_complete(
+asyncio.get_event_loop_policy().get_event_loop().run_until_complete(
     websockets.serve(handle, '0.0.0.0', 8765))
-asyncio.get_event_loop().run_forever()
+
+# asyncio.get_event_loop().run_until_complete(websockets.serve(handle, '0.0.0.0', 8765))
+# asyncio.get_event_loop().run_forever()
